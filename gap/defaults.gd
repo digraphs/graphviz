741fdf48e15f74cb63965a83eaeeccf9d1baa319
mpan322 <mp322@st-andrews.ac.uk>
2024-01-30 10:14:15 +0000

    updated docs!

--- gap/defaults.gd
+++ gap/defaults.gd
@@ -33,12 +33,52 @@ DeclareOperation("GV_DotEdgeColoredDigraph", [IsDigraph, IsList]);
 #! and labels it's vertices with the provided labels.
 DeclareOperation("GV_DotVertexLabelledDigraph", [IsDigraph]);
 
+#! @Arguments graph
+#! @Returns the graphviz representation of the graph. 
+#! @Description Converts a graph to it's graphviz representation.
 DeclareAttribute("GV_DotSymmetricDigraph", IsDigraph);
+
+#! @Arguments graph, vertex_colors, edge_colors
+#! @Returns the graphviz representation of the graph with the provided colors. 
+#! @Description Converts a graph to it's graphviz representation and colors 
+#! the edges and verticies using the colors provided.
 DeclareOperation("GV_DotSymmetricColoredDigraph", [IsDigraph, IsList, IsList]);
+
+#! @Arguments graph, vertex_colors
+#! @Returns the graphviz representation of the graph. 
+#! @Description Converts a graph to it's graphviz representation 
+#! and colors it's verticies with it's provided vertex colors.
 DeclareOperation("GV_DotSymmetricVertexColoredDigraph", [IsDigraph, IsList]);
+
+#! @Arguments graph, vertex_colors
+#! @Returns the graphviz representation of the graph. 
+#! @Description Converts a graph to it's graphviz representation 
+#! and colors it's edges with the provided edge colors.
 DeclareOperation("GV_DotSymmetricEdgeColoredDigraph", [IsDigraph, IsList]);
-DeclareAttribute("GV_DotPartialOrderDigraph", IsDigraph);
+
+#! @Arguments digraph, vertex_colors
+#! @Returns the graphviz representation of a partial order digraph. 
+#! @Description Converts a partial order digraph to it's graphviz representation 
+DeclareOperation("GV_DotPartialOrderDigraph", [IsDigraph]);
+
+#! @Arguments digraph
+#! @Returns the graphviz representation of a preorder digraph. 
+#! @Description Converts a preorder digraph to it's graphviz representation 
 DeclareAttribute("GV_DotPreorderDigraph", IsDigraph);
+
+# TODO GAP AUTODOC DOES NOT HAVE SUPPORT FOR SYNONYMS - ASK SUPERVISORS
+# CURRENT IDEA IS TO MODIFY THE AUTODOC CODE TO DO IT - HAVE BASIC VERSION
+# BUT NEEDS WORK!
 DeclareSynonym("GV_DotQuasiorderDigraph", DotPreorderDigraph);
+
+#! @Arguments digraph, hight_verts
+#! @Returns the graphviz representation of a digraph with the 
+#! highlighted verticies in black and the others in grey. 
+#! @Description Converts a highlighted digraph to it's graphviz representation 
 DeclareOperation("GV_DotHighlightedDigraph", [IsDigraph, IsList]);
+
+#! @Arguments digraph, hight_verts, hight_color, low_color
+#! @Returns the graphviz representation of a digraph with the 
+#! highlighted verticies in the high_color and the others in the low_color. 
+#! @Description Converts a highlighted digraph to it's graphviz representation 
 DeclareOperation("GV_DotHighlightedDigraph", [IsDigraph, IsList, IsString, IsString]);
